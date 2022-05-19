d7970a3d17e9ccf65f9bf4635cfa16a6de0a112e
Nguyễn Huy Hoà <81904865+hoanguyen6611@users.noreply.github.com>
2022-05-19 16:38:54 +0700

    post

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import RegisterHook from "./components/form/RegisterHook";
+function App() {
+  return (
+    <div className="App">
+      <RegisterHook></RegisterHook>
+    </div>
+  );
+}
+
+export default App;
